ee530b03665f6dc2b53f608c174f7955de7e85e7
Yitzhak Amsalem <yitzhak@gmail.com>
2021-06-23 10:31:27 +0300

    3 LEG

--- web-scraping-master/src/main/java/Main.java
+++ web-scraping-master/src/main/java/Main.java
@@ -1,7 +1,13 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         YnetRobot ynetRobot = new YnetRobot();
-        ynetRobot.getWordsStatistics();
+        HashMap <String, Integer> wordsMap = new HashMap<>();
+        wordsMap = (HashMap<String, Integer>) ynetRobot.getWordsStatistics();
+        System.out.println("im: ");
+        System.out.println(wordsMap.get("אם"));
+
 
     }
 }
--- web-scraping-master/src/main/java/YnetRobot.java
+++ web-scraping-master/src/main/java/YnetRobot.java
@@ -24,7 +24,7 @@ public class YnetRobot extends BaseRobot {
         try {
             Document website = Jsoup.connect(getRootWebsiteUrl()).get();
 
-            Elements mainArticles5 = website.getElementsByAttribute("href");
+            Elements allLinks = website.getElementsByAttribute("href");
 
 //            Element linkElement = mainArticles5.get(0);
 //            String link1 = linkElement.attr("href");
@@ -34,17 +34,16 @@ public class YnetRobot extends BaseRobot {
 //            classALL19 = layoutContainer
 //            classB = tbl-feed-container tbl-feed-frame-NONE  render-late-effect
 //            classA = trc_related_container trc_spotlight_widget tbl-rtl tbl-feed-container tbl-feed-frame-NONE
-            System.out.println("Found " + mainArticles5.size());
+//            System.out.println("Found " + mainArticles5.size());
 
             StringBuilder allText = new StringBuilder();
-            for (int i = 0; i < mainArticles5.size(); i++){
-                Element linkElement = mainArticles5.get(i);
+            for (int i = 0; i < allLinks.size(); i++){
+                Element linkElement = allLinks.get(i);
                 String link = linkElement.attr("href");
                 if (link.contains("https://www.ynet.co.il/judaism/article/r1PEIlTsO#autoplay")){
                 }
-                if (i != mainArticles5.size()-1) {
-                    if (link.contains("https://www.ynet.co.il/") && !link.equals(mainArticles5.get(i + 1).attr("href")) && scanLink(link) > 4) {
-//                        Thread.sleep(1000);
+                if (i != allLinks.size()-1) {
+                    if (link.contains("https://www.ynet.co.il/") && !link.equals(allLinks.get(i + 1).attr("href")) && scanLink(link) > 4) {
                         Document ynetPage = Jsoup.connect(link).get();
                         Elements mainTitle = ynetPage.getElementsByClass("mainTitle");
                         Elements subTitle = ynetPage.getElementsByClass("subTitle");
@@ -53,46 +52,44 @@ public class YnetRobot extends BaseRobot {
                         allText.append(subTitle.text());
                         for (Element element : textPage) {
                             allText.append(element.text());
-//                            System.out.println(element.text());
                         }
-//                        System.out.println("Link: " + link);
-//                        System.out.println("Article: " + linkElement.text());
                         size++;
                     }
-
                 }
-
             }
 
-//            class = mainTitle
-//            class = subTitle
-//            class = text_editor_paragraph rtl
-//            key = data-text
-
-
-
-//            for (Element element : mainArticles5) {
-//                String link = element.attr("href");
-//                if (link.contains("www.ynet.co.il")) {
-//
-//                }
-//                if (link.contains("www.ynet.co.il") && size == 0){
-//                    System.out.println("Link: " + link);
-//                    System.out.println("Article: " + element.text());
-//                    size = 0;
-//                }
-//            }
             System.out.println(size);
-            System.out.println(allText);
-
+            System.out.println(allText.length());
+            String word = "";
+            for (int i = 0; i<allText.length(); i++){
+                char chekWord = allText.charAt(i);
+                if ((chekWord > 1487 && chekWord < 1515) || chekWord == 34 ||
+                        (chekWord >= '0' && chekWord <= '9') || (chekWord >= 'A' && chekWord <= 'Z') || (chekWord >= 'a' && chekWord <= 'z')){
+                    if (!(chekWord == 34)) {
+                        word += chekWord;
+                    }
+                }
+                else {
+                    if (word.length() != 0) {
+                        if (wordsMap.get(word) != null) {
+                            Integer value = wordsMap.get(word) + 1;
+                            wordsMap.put(word, value);
+                        }
+                        else {
+                            wordsMap.put(word, 1);
+                        }
+                    }
+                    word = "";
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        return null;
+        return wordsMap;
     }
+
     public int scanLink (String linkToScan){
         int count = 0;
         for (int i = 0; i < linkToScan.length(); i++){
